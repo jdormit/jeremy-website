ef738f6cbc8f40e9ba0575c0d729feb43e13a62e
Jeremy Dormitzer <jeremydormitzer@lola.com>
2020-10-08 13:47:23 -0400

    Link to Twitter instead of Mastodon

--- src/index.html.pm
+++ src/index.html.pm
@@ -8,4 +8,4 @@ Hi! I’m Jeremy Dormitzer. I’m a full-stack software engineer from Boston, MA
 
 I love generative art, entrepreneurship, and automation. I think that software gives us superpowers, and I want to build programs that suprise, delight, and empower.
 
-Connect with me on ◊link[#:href "https://mastodon.technology/@jdormit"]{Mastodon} or ◊link[#:href "https://linkedin.com/in/jeremydormitzer"]{LinkedIn}.
+Connect with me on ◊link[#:href "https://twitter.com/jeremydormitzer"]{Twitter} or ◊link[#:href "https://linkedin.com/in/jeremydormitzer"]{LinkedIn}.
